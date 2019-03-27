286940b58097b4407609c103f58d4bc1fa506571
徐夏冰 <037333@gmail.com>
2019-03-27 14:58:31 +0800

    ide adapt.

--- src/JPushServiceFacade.php
+++ src/JPushServiceFacade.php
@@ -4,6 +4,15 @@ namespace Mitoop\JPush;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static JPushService setPlatform(string|array $platform)
+ * @method static JPushService toAll();
+ * @method static JPushService toAlias(string|array $alias)
+ * @method static JPushService toTag(string|array $tag)
+ * @method static JPushService notify(string $notification)
+ * @method static mixed send()
+ * @method static JPushService extras(array $extras)
+ */
 class JPushServiceFacade extends Facade
 {
     /**
